d43e6df55742521d57f792d6490b06952d28536f
Anthony Barragan <anthony.bar.89@gmail.com>
2024-04-10 20:51:16 -0700

    database creation on first time users is now working

--- utils/db/init.js
+++ utils/db/init.js
@@ -5,102 +5,82 @@ const db = SQLite.openDatabase('cannonBallDB', 1);
 export async function initDB() {
     await db.transactionAsync(async tx => {
 
-
-
         // check to see if there is something in the db already
         const isAccountMade = await tx.executeSqlAsync(`
             SELECT * FROM users;
         `)
 
-        console.log('lenght of rows', isAccountMade)
         // Only create new tables and new user if new user doesn't already exist. 
         if (isAccountMade.rows.length === 0) {
-            /////////// Create Map Table ///////////
-            await tx.executeSqlAsync(`
-                CREATE TABLE IF NOT EXISTS maps (
+            try {
+                ///////// Create Preferences Table ///////////
+                await tx.executeSqlAsync(`
+                CREATE TABLE IF NOT EXISTS preferences (
                     id INTEGER PRIMARY KEY NOT NULL,
-                    mapName TEXT,
-                    earnedStars INTEGER DEFAULT 0,
-                    requiredStars INTEGER
-                );  
-            `, []);
-            // Create maps
-            const { insertId: mapOneId } = await tx.executeSqlAsync(`
-                INSERT INTO maps (mapName, requiredStars) VALUES ('basics', 0);
-            `, [])
-
-            console.log('mapOneId', mapOneId)
-            //////////// Crate Level Table ///////////////
-            await tx.executeSqlAsync(`
+                        soundOn INTEGER DEFAULT 1,
+                        soundEffectsOn INTEGER DEFAULT 1,
+                        hasSeenTutorial INTEGER DEFAULT 0
+                    );  
+                    `, []);
+                ///////// Create User Table ///////////////
+                await tx.executeSqlAsync(`
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        name TEXT,
+                        totalPoints INTEGER DEFAULT 0,
+                        totalStars INTEGER DEFAULT 0,
+                        preference_id INTEGER,
+                        FOREIGN KEY (preference_id) REFERENCES preferences(id) ON DELETE CASCADE
+                    );
+                    `, []);
+                /////////// Create Map Table ///////////
+                await tx.executeSqlAsync(`
+                    CREATE TABLE IF NOT EXISTS maps (
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        mapName TEXT,
+                        earnedStars INTEGER DEFAULT 0,
+                        requiredStars INTEGER,
+                        user_id INTEGER,
+                        FOREIGN KEY (user_id) REFERENCES users(id)
+                    );  
+                    `, []);
+                //////////// Create Level Table ///////////////
+                await tx.executeSqlAsync(`
                 CREATE TABLE IF NOT EXISTS levels (
-                    id INTEGER PRIMARY KEY NOT NULL,
-                    level INTEGER,
-                    accuracy INTEGER DEFAULT 0,
-                    highscore INTEGER DEFAULT 0,
-                    passed INTEGER DEFAULT 0,
-                    earnedStars INTEGER DEFAULT 0,
-                    mapId INTEGER,
-                    FOREIGN KEY (mapId) REFERENCES maps(id)
-                );
-            `, [])
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        level INTEGER,
+                        accuracy INTEGER DEFAULT 0,
+                        highscore INTEGER DEFAULT 0,
+                        passed INTEGER DEFAULT 0,
+                        earnedStars INTEGER DEFAULT 0,
+                        mapId INTEGER,
+                        FOREIGN KEY (mapId) REFERENCES maps(id)
+                    );
+                    `, [])
 
-            // Create Level
-            const levelCreated = await tx.executeSqlAsync(`
-                INSERT INTO levels (level, mapId) VALUES (1, ${mapOneId});
-            `, []);
+                // Set Default Preferences
+                const { insertId: preferenceId } = await tx.executeSqlAsync(`INSERT INTO preferences DEFAULT VALUES;`, []);
+                // Create User
+                const { insertId: newUserId } = await tx.executeSqlAsync(`INSERT INTO users (name, preference_id) VALUES ('mike', ${preferenceId});`, []);
+                // Create Maps
+                const { insertId: mapOneId } = await tx.executeSqlAsync(`INSERT INTO maps (mapName, requiredStars, user_id) VALUES ('basics', 0 ${newUserId});`, []);
+                // Create Level
+                await tx.executeSqlAsync(`INSERT INTO levels (level, mapId) VALUES (1, ${mapOneId});`, []);
 
-            console.log('levelcreated', levelCreated)
-
-            // check to see if level is there
-            const level = await tx.executeSqlAsync(`
-                SELECT m.mapName, m.id, l.level, l.passed FROM levels l LEFT JOIN maps m ON l.mapId=m.id; 
-            `)
-
-            console.log('level', level)
-
-
-            /////////// Create Preferences Table ///////////
-            await tx.executeSqlAsync(`
-            CREATE TABLE IF NOT EXISTS preferences (
-                id INTEGER PRIMARY KEY NOT NULL,
-                soundOn INTEGER DEFAULT 1,
-                soundEffectsOn INTEGER DEFAULT 1,
-                hasSeenTutorial INTEGER DEFAULT 0
-                );  
-                `, []);
-            // Set Default Preferences
-            const { insertId: preferenceId } = await tx.executeSqlAsync(`
-                INSERT INTO preferences DEFAULT VALUES;
-            `)
-
-
-            /////////// Create User Table ///////////////
-            await tx.executeSqlAsync(`
-                CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY NOT NULL,
-                    name TEXT,
-                    totalPoints INTEGER DEFAULT 0,
-                    totalStars INTEGER DEFAULT 0,
-                    preferenceId INTEGER,
-                    FOREIGN KEY (preferenceId) REFERENCES preferences(id) ON DELETE CASCADE
-                );
-            `, []);
-            // Create User
-            await tx.executeSqlAsync(`  
-                INSERT INTO users (name, totalPoints, totalStars, preferenceId) 
-                    VALUES('mike', 1, 1, ${preferenceId});
-            `, [])
+            } catch (error) {
+                console.log('error in Init SQL function', error)
+            }
         }
 
 
-
-
-        // await tx.executeSqlAsync(`
-        //  DELETE FROM users;  
-        //  DELETE FROM maps;
-        //  DELETE FROM levels;  
-        // `)
-
+        // try {
+        //     await tx.executeSqlAsync(`DELETE FROM users;`, [])
+        //     await tx.executeSqlAsync(`DELETE FROM preferences;`, [])
+        //     await tx.executeSqlAsync(`DELETE FROM maps;`, [])
+        //     await tx.executeSqlAsync(`DELETE FROM levels;`, [])
+        // } catch (error) {
+        //     console.log('error in deleting ', error)
+        // }
 
     })
 }
@@ -108,13 +88,20 @@ export async function initDB() {
 export async function getPreferences() {
     // Delete FROM preferences WHERE hasSeenTutorial=0;
     await db.transactionAsync(async tx => {
-        const result = await tx.executeSqlAsync(`
-            SELECT u.name, u.totalPoints, p.soundOn, p.hasSeenTutorial FROM users u LEFT JOIN preferences p ON u.preference_id=p.id;
-        `)
-        console.log('results ', result)
-        result.rows.forEach((row) => {
-            console.log('users', row)
-        });
+        try {
+            const result = await tx.executeSqlAsync(`
+                SELECT u.name, u.totalPoints, p.soundOn, p.hasSeenTutorial FROM users u LEFT JOIN preferences p ON u.preference_id=p.id;
+            `)
+            // const result = await tx.executeSqlAsync(`
+            //     SELECT * from users;
+            // `)
+            console.log('results ', result)
+            result.rows.forEach((row) => {
+                console.log('users', row)
+            });
+        } catch (error) {
+            console.log('error in get preference ', error)
+        }
     })
 }
 
