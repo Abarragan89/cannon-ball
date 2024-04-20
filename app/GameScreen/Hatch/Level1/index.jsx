1d25bbcfc4a47664e021ce5ced1db874bd6825be
Anthony Barragan <anthony.bar.89@gmail.com>
2024-04-20 16:34:15 -0700

    Added backend routes to all maps and levels. Added additional conditioning for final level so the 'next level' button doesn't appear

--- app/GameScreen/Hatch/Level1/index.jsx
+++ app/GameScreen/Hatch/Level1/index.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect, useContext } from "react";
-import { GameEngine } from "react-native-game-engine"
+import { GameEngine } from "react-native-game-engine";
+import { useLocalSearchParams } from 'expo-router';
 import { StyleSheet, StatusBar, ImageBackground } from 'react-native';
 import cannonControlSystem from "../../../../systems/cannonControlSystem";
 import fireCannonSystem from "../../../../systems/fireCannonSystem";
@@ -28,8 +29,19 @@ import HatchBox from "../../../../Components/GameEngine/HatchBox";
 import hatchBoxDetectionSystem from "../../../../systems/hatchDetectionSystems/hatchBox.Detection";
 import hatchLidDetectionSystem from "../../../../systems/hatchDetectionSystems/hatchLid.Detection";
 import { SoundContext } from "../../../../store/soundsContext";
+import { getIndividualLevelData } from "../../../../utils/db/selectQueries";
+import {
+    updateLevelToPass,
+    updateLevelHighScore,
+    updateLevelAccuracy,
+    updateUserTotalPoints,
+    updateLevelEarnedStars
+} from "../../../../utils/db/updateQueries";
 
 function ChapterFiveLevelOne() {
+    // Grab the level Id 
+    const { levelId, lastAccuracy, lastHighscore, lastEarnedStars } = useLocalSearchParams();
+
     // Load sounds from context API, make gameEngineRef, and gameOver State
     const { sounds: gameSoundContext } = useContext(SoundContext);
     const gameEngineRef = useRef(null);
@@ -82,9 +94,63 @@ function ChapterFiveLevelOne() {
         multiplier: 0,
         currentLevel: 'Hatch',
         nextLevel: 'Hatch/Level2'
-    })
-    return (
+    });
 
+    // Backend updates 
+    useEffect(() => {
+        // 'isGameOver' should more appropriately be named 'gameWon'
+        if (isGameOver) {
+            // get highscore, accuracy, and earnedStars amount after user wins
+            const currentHighScore = endGameData.current.multiplier * (endGameData.current.airTime * endGameData.current.bounces)
+            const currentAccuracy = endGameData.current.accuracyFloat;
+            let currentEarnedStars = 0
+            // determine earned stars
+            if (currentHighScore >= endGameData.current.winningScore[2]) {
+                currentEarnedStars = 3;
+            } else if (currentHighScore >= endGameData.current.winningScore[1]) {
+                currentEarnedStars = 2;
+            } else if (currentHighScore >= endGameData.current.winningScore[0]) {
+                currentEarnedStars = 1;
+            } else {
+                currentEarnedStars = 0;
+            }
+            async function updateLevelData() {
+                // Update level to passed if not already passed
+                await updateLevelToPass(levelId)
+                // Update users highscore
+                await updateUserTotalPoints(currentHighScore)
+                // Compare the highscore to the old highscore
+                if (currentHighScore > +lastHighscore) {
+                    await updateLevelHighScore(levelId, currentHighScore)
+                }
+                // Compare the accuracy with old accuracy
+                if (currentAccuracy < +lastAccuracy) {
+                    await updateLevelAccuracy(levelId, currentAccuracy)
+                }
+                // Compare earnedStars
+                if (currentEarnedStars > +lastEarnedStars) {
+                    await updateLevelEarnedStars(levelId, currentEarnedStars)
+                }
+            }
+            updateLevelData();
+        }
+    }, [isGameOver, endGameData.current]);
+
+    const [nextLevelData, setNextLevelData] = useState(null);
+
+    // Get next level information to pass as params in the 
+    // next level button in the end of game modal
+    useEffect(() => {
+        async function getNextLevelData() {
+            const mapName = endGameData.current.nextLevel.split('/')[0];
+            const link = endGameData.current.nextLevel.split('/')[1];
+            const nextLevel = await getIndividualLevelData(mapName, link)
+            setNextLevelData(nextLevel[0])
+        }
+        getNextLevelData();
+    }, []);
+
+    return (
         <ImageBackground
             source={require('../../../../assets/images/basics/level1.png')}
             style={styles.backgroundImg}
@@ -198,6 +264,7 @@ function ChapterFiveLevelOne() {
                 {isGameOver &&
                     <EndGameModal
                         endGameData={endGameData}
+                        nextLevelData={nextLevelData}
                     />
                 }
             </GameEngine>
