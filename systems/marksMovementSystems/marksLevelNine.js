5a77235a36e8dade9dfcf4e7043dd9ce7c41077a
Anthony Barragan <anthony.bar.89@gmail.com>
2024-06-14 13:19:28 -0700

    Added initial logic to level 9 marks. Need to fix the pause

--- systems/marksMovementSystems/marksLevelNine.js
+++ systems/marksMovementSystems/marksLevelNine.js
@@ -1,24 +1,96 @@
+const marksNineSpeed = 2;
+const marksNineTimeDelay = 0.05
+
 const moveTNTMarksLevelNine = (entities) => {
+    const moveDiagonalDownLeft = () => {
+        entities.TNT.position[0] -= marksNineSpeed;
+        entities.TNT.position[1] += marksNineSpeed;
+        entities.tntMovementCount.tntPixelCounter += marksNineSpeed;
+    }
+    const moveDiagonalUpLeft = () => {
+        entities.TNT.position[0] -= marksNineSpeed;
+        entities.TNT.position[1] -= marksNineSpeed;
+        entities.tntMovementCount.tntPixelCounter += marksNineSpeed;
+    }
+    const moveDiagonalDownRight = () => {
+        entities.TNT.position[0] += marksNineSpeed;
+        entities.TNT.position[1] += marksNineSpeed;
+        entities.tntMovementCount.tntPixelCounter += marksNineSpeed;
+    }
+    const moveDiagonalUpRight = () => {
+        entities.TNT.position[0] += marksNineSpeed;
+        entities.TNT.position[1] -= marksNineSpeed
+        entities.tntMovementCount.tntPixelCounter += marksNineSpeed;
+    }
+
     if (!entities.gameData.isGameOver) {
-        const speed = 2;
+        // PAUSE
+        if (entities.tntMovementCount.tntPixelCounter < 1) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
         // Move Diagonal Down
-        if (entities.tntMovementCount.tntPixelCounter <= 150) {
-            entities.TNT.position[0] -= speed;
-            entities.TNT.position[1] += speed 
-            entities.tntMovementCount.tntPixelCounter += speed;
-            // Move Left
-        } else if (entities.tntMovementCount.tntPixelCounter > 150 && entities.tntMovementCount.tntPixelCounter <= 300) {
-            entities.TNT.position[0] -= speed;
-            entities.tntMovementCount.tntPixelCounter += speed
-            // moving left
-        } else if (entities.tntMovementCount.tntPixelCounter > 450 && entities.tntMovementCount.tntPixelCounter <= 750) {
-            entities.TNT.position[0] -= speed;
-            entities.tntMovementCount.tntPixelCounter++;
-            // moving up
-        } else if (entities.tntMovementCount.tntPixelCounter > 750 && entities.tntMovementCount.tntPixelCounter <= 900) {
-            entities.TNT.position[1] -= speed;
-            entities.tntMovementCount.tntPixelCounter++;
-        } else if (entities.tntMovementCount.tntPixelCounter > 900) {
+        else if (entities.tntMovementCount.tntPixelCounter > 2 && entities.tntMovementCount.tntPixelCounter <= 200) {
+            moveDiagonalDownLeft();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 200 && entities.tntMovementCount.tntPixelCounter <= 202) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Diagonal Up
+        else if (entities.tntMovementCount.tntPixelCounter > 202 && entities.tntMovementCount.tntPixelCounter <= 400) {
+            moveDiagonalUpLeft();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 400 && entities.tntMovementCount.tntPixelCounter <= 402) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Right
+        else if (entities.tntMovementCount.tntPixelCounter > 402 && entities.tntMovementCount.tntPixelCounter <= 600) {
+            moveDiagonalDownLeft();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 600 && entities.tntMovementCount.tntPixelCounter <= 602) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Down Right Diagonal
+        else if (entities.tntMovementCount.tntPixelCounter > 600 && entities.tntMovementCount.tntPixelCounter <= 800) {
+            moveDiagonalUpLeft();
+        }
+
+        //////////// REVERSE ///////////////
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 800 && entities.tntMovementCount.tntPixelCounter <= 802) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Diagonal Up
+        else if (entities.tntMovementCount.tntPixelCounter > 802 && entities.tntMovementCount.tntPixelCounter <= 1000) {
+            moveDiagonalDownRight();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 1000 && entities.tntMovementCount.tntPixelCounter <= 1002) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Right
+        else if (entities.tntMovementCount.tntPixelCounter > 1002 && entities.tntMovementCount.tntPixelCounter <= 1200) {
+            moveDiagonalUpRight();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 1200 && entities.tntMovementCount.tntPixelCounter <= 1202) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Down Right Diagonal
+        else if (entities.tntMovementCount.tntPixelCounter > 1202 && entities.tntMovementCount.tntPixelCounter <= 1400) {
+            moveDiagonalDownRight();
+        }
+        // PAUSE
+        else if (entities.tntMovementCount.tntPixelCounter > 1400 && entities.tntMovementCount.tntPixelCounter <= 1402) {
+            entities.tntMovementCount.tntPixelCounter += marksNineTimeDelay
+        }
+        // Move Up Left Diagonal
+        else if (entities.tntMovementCount.tntPixelCounter > 1402 && entities.tntMovementCount.tntPixelCounter <= 1600) {
+            moveDiagonalUpRight();
+        }
+        else if (entities.tntMovementCount.tntPixelCounter > 1600) {
             entities.tntMovementCount.tntPixelCounter = 0;
         }
     }
